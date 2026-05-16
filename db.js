69324e86be9cd08c259e2952898bb5cde73e9081
AlbernazFernando <fernandoalbernaz415@gmail.com>
2026-05-16 09:54:18 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2')
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'catolica',
+  database: 'userdb',
+  port:3307
+})
+
+db.connect(err =>{
+    if (err) throw err;
+    console.log('conectado ao banco de dados')
+})
+module.exports = db.js
+
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const db = require('./db')
+app.get('/', (req, res) => {
+  res.sendfile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
